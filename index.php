aaa9649b6dd0fd51ed680454bc8a37fae1087b80
dimaswahyuardhana <dimasardhana29@gmail.com>
2023-02-24 20:27:44 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,100&display=swap" rel="stylesheet">
+        <style>
+            body{
+                font-family: 'poppins', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="card mb-3">
+                <div class="card-body p-3">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et at similique officia quidem dolore explicabo itaque molestias est magni. Eum fugiat vitae blanditiis unde, suscipit cumque, assumenda rerum veritatis nulla fuga eligendi debitis expedita nihil, a architecto odio mollitia consequatur sequi ipsum itaque. Iste libero odit doloribus cumque aperiam eos!
+                    </p>
+                    <div class="d-flex">
+                        <button type="button" class="btn btn-primary ms-auto">Details</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3">
+                <div class="card-body p-3">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et at similique officia quidem dolore explicabo itaque molestias est magni. Eum fugiat vitae blanditiis unde, suscipit cumque, assumenda rerum veritatis nulla fuga eligendi debitis expedita nihil, a architecto odio mollitia consequatur sequi ipsum itaque. Iste libero odit doloribus cumque aperiam eos!
+                    </p>
+                </div>
+            </div>
+
+            <div class="card mb-3">
+                <div class="card-body p-3">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et at similique officia quidem dolore explicabo itaque molestias est magni. Eum fugiat vitae blanditiis unde, suscipit cumque, assumenda rerum veritatis nulla fuga eligendi debitis expedita nihil, a architecto odio mollitia consequatur sequi ipsum itaque. Iste libero odit doloribus cumque aperiam eos!
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </body>
+</html>
